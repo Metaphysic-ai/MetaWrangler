07fae48ccafc9a8495fdb33fed717bfd853b0ccd
ottokopp <kopp.otto@gmail.com>
2024-05-02 06:26:22 +0200

    updated idle check to not remove workers that never rendered anything

--- MetaWrangler.py
+++ MetaWrangler.py
@@ -102,7 +102,6 @@ class MetaWrangler():
 
     def is_worker_idle(self, worker, delta_min=5):
         worker_db = wrangler.get_worker_db(worker)
-        print("### DEBUG:", worker_db)
 
         last_render_time_str = worker_db["info"]["LastRenderTime"]
         if last_render_time_str is None:
