801efaba71a21abbb5369e3d8c1135f2bcec36f6
ottokopp <kopp.otto@gmail.com>
2024-05-06 07:10:27 +0200

    renderserver debug extravaganza 3

--- MetaWrangler.py
+++ MetaWrangler.py
@@ -269,6 +269,7 @@ class MetaWrangler():
 
         mng = ContainerManager(self)
         self.create_task_event(id=0, mem=16, cpus=16, gpu=False, batch_size=10, timeout=10, creation_time=datetime.now())
+        print("### DEBUG: Numbers of tasks in stack:", len(self.task_event_stack))
 
         while True:
             # print(self.get_running_jobs())  # Execute your periodic task
@@ -292,6 +293,7 @@ class MetaWrangler():
                     self.task_event_stack.pop(0)
                 else:
                     self.task_event_stack.append(self.task_event_stack.pop(0)) ### put task to the end of the stack in case one gets stuck
+            print("### DEBUG: Numbers of tasks in stack:", len(self.task_event_stack))
 
 
 if __name__ == "__main__":
