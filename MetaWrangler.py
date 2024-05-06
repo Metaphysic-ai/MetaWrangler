2ee9205a761fd5570d0a433e6dc81ac0b50530ce
ottokopp <kopp.otto@gmail.com>
2024-05-06 09:00:00 +0200

    renderserver debug extravaganza 5

--- MetaWrangler.py
+++ MetaWrangler.py
@@ -129,8 +129,9 @@ class MetaWrangler():
         if minutes_difference > delta_min:
             return False
 
-        elif not worker_db["StatDate"]:
-            last_render_time_str = worker_db["StatDate"]
+        elif not worker_db["info"]:
+            if len(worker_db["info"]):
+                last_render_time_str = worker_db["StatDate"]
 
         elif last_render_time_str is None:
             return False
