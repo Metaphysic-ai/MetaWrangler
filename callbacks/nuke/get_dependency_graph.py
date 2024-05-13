8c208bc523225427a5230a28e9cc074e9e8b33e8
ottokopp <kopp.otto@gmail.com>
2024-05-13 02:59:19 +0200

    debugsesh

--- callbacks/nuke/get_dependency_graph.py
+++ callbacks/nuke/get_dependency_graph.py
@@ -4,32 +4,37 @@ import os
 
 payload_dict = {nuke.root().name(): {}}
 write_nodes = []
-[write_nodes.append(node) for node in nuke.allNodes("Write", recurseGroups=True)]
+for node in nuke.allNodes("Write", recurseGroups=True):
+    write_nodes.append(node)
+for node in nuke.allNodes("WriteTank", recurseGroups=True):
+    write_nodes.append(node)
 
-seen = set()
-stack = [node]
-nodes = set()
+def get_dependencies(node, write_nodes):
+    seen = set()
+    stack = [node]
+    nodes = set()
 
-while stack:
-current_node = stack.pop()
-if current_node not in seen:
-seen.add(current_node)
-node_class = current_node.Class()
-node_name = current_node.name()
+    while stack:
+        current_node = stack.pop()
+        if current_node not in seen:
+            seen.add(current_node)
+            node_class = current_node.Class()
+            node_name = current_node.name()
 
-if (node_class, node_name) != (node.Class(), node.name()):
-if node_class not in ['Dot', 'BackdropNode']:
-nodes.add((node_class, node_name))
+            if (node_class, node_name) != (node.Class(), node.name()):
+                if node_class not in ['Dot', 'BackdropNode']:
+                    nodes.add((node_class, node_name))
 
-if node_class not in ['Write', 'WriteTank'] or current_node == node:
-for dep in current_node.dependencies():
-if dep not in seen:
-stack.append(dep)
-else:
-if current_node in write_nodes and current_node != node:
-nodes.add((node_class, node_name))
+            if node_class not in ['Write', 'WriteTank'] or current_node == node:
+                for dep in current_node.dependencies():
+                    if dep not in seen:
+                        stack.append(dep)
+            else:
+                if current_node in write_nodes and current_node != node:
+                    nodes.add((node_class, node_name))
+    return nodes
 
 for node in write_nodes:
-payload_dict[nuke.root().name()][node.name()] = get_dependencies(node, write_nodes)
+    payload_dict[nuke.root().name()][node.name()] = get_dependencies(node, write_nodes)
 
 print(payload_dict)
